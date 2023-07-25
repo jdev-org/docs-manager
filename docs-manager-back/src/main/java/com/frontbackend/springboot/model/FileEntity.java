d3dc471ee84b18652fa613ed21f96f62f5b8e199
gaetanbrl <gaetan.bruel@jdev.fr>
2023-07-25 09:12:37 +0200

    add submodule

---
+++ docs-manager-back/src/main/java/com/frontbackend/springboot/model/FileEntity.java
@@ -0,0 +1,87 @@
+package com.frontbackend.springboot.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "FILES")
+public class FileEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    private String name;
+
+    private String contentType;
+    private String label;
+    private String id_entity;
+    private String id_plugin;
+
+    private Long size;
+
+    @Lob
+    private byte[] data;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getIdEntity() {
+        return id_entity;
+    }
+
+    public String getIdPluign() {
+        return id_plugin;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+}
